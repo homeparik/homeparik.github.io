62888415b4665d636ed7b4f089cdf38b2aaaf790
ankparik <ankparik@cisco.com>
2016-07-25 14:33:55 -0400

    Initial push

---
+++ fileserver/lunchDinner.js
@@ -0,0 +1,9 @@
+function getLunch() {
+  var lunch = ["Rotli", "Tindora Bataka Shak", "Dal", "Bhat"];
+  return lunch;
+}
+
+function getDinner() {
+  var dinner = ["Bhakhri", "Brocolli Shak", "Dal", "Bhat"];
+  return dinner;
+}
